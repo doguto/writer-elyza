2ddb0a2873253a4d64271144e238850690b55b5c
doguto <doguto110@gmail.com>
2024-12-10 12:04:12 +0900

    [Feat] setup chat-system using elyza

--- src/writer/elyza.py
+++ src/writer/elyza.py
@@ -1,15 +1,32 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
+from llama_cpp import Llama
 
-
-def initialize_elyza():
-    tokenizer = AutoTokenizer.from_pretrained("elyza/ELYZA-japanese-Llama-2-7b-instruct")
-    model = AutoModelForCausalLM.from_pretrained(
-        "elyza/ELYZA-japanese-Llama-2-7b-instruct",
-        torch_dtype=torch.float16,
-        device_map="auto",
-    )   
+class Elyza():
+    def __init__(self):
+        self.llm = Llama(
+            model_path="../../elyza-models/Llama-3-ELYZA-JP-8B-q4_k_m.gguf",  # ダウンロードしたモデルのパス
+            chat_format="llama-3",  # Llama 3のフォーマット指定
+            n_ctx=1024,  # コンテキストの長さ
+        )
+    
+    def get_responce(self, input:str):
+        response = self.llm.create_chat_completion(
+            messages=[
+                {
+                    "role": "system",
+                    "content": "あなたは誠実で優秀な日本人のアシスタントです。特に指示が無い場合は、常に日本語で回答してください。",
+                },
+                {
+                    "role": "user",
+                    "content": input,
+                },
+            ],
+            max_tokens=1024,  # 最大トークン数
+        )
+        
+        # 結果の出力
+        print(response["choices"][0]["message"]["content"])
 
 
 if __name__ == '__main__':
-    initialize_elyza()
+    elyza = Elyza()
+    elyza.get_responce("カワウソについて、教えてください。")
