91cfae252a7dc89c06ff46b4b6097b9815901273
doguto <doguto110@gmail.com>
2024-12-22 17:35:53 +0900

    [Feat] BasicInfo, StoryInfo class

--- src/novel/novel.py
+++ src/novel/novel.py
@@ -1,32 +1,87 @@
+
+class Story():
+    chapter = 0
+    title = ''
+    text:str = ''
+    isPublished = False
+    
+    def set_chapter(self, chapter:int):
+        self.chapter = chapter
+
+    def set_title(self, title:str):
+        self.title = title
+
+    def set_text(self, text:str):
+        self.text = text
+
+    def add_text(self, text:str):
+        self.text += '\n'
+        self.text += text
+
+    def publish(self):
+        self.isPublished = True
+
+
+
+class BasicInfo():
+    def __init__(self, title='', description='', genre=1, category=110400, length=3, state=2, regulation=0):
+        self.title = title
+        self.description = description
+
+        # 0: no selection  # 1: for men  # 2: for wemen
+        self.genre:int = genre
+        
+        # 110100: mistery   # 110200: horror     # 110300: SF       # 110400: fantasy
+        # 110500: love      # 111400: character  # 111500: light    # 110600: blue-spring
+        # 110700: current   # 110800: masses     # 111000: economy  # 111100: history
+        # 111200: children  # 111300: picture    # 119000: BL       # 111600: essay
+        self.category:int = category
+
+        # 1: short-short  # 2: short  # 3: long
+        self.length = length
+
+        # 1: completed  # 2: writing now
+        self.state = state
+
+        # 0: no regulation  # 1: R-15  # 2: R-18
+        self.regulation = regulation
+
+        # can't set these parameters in constructer
+        self.tags:list[str] = []
+        self.isPublished = False
+
+
+    def set_title(self, title:str):
+        self.title = title
+
+    def set_description(self, description:str):
+        self.description = description
+    
+    def set_genre(self, genre:int):
+        self.genre = genre
+
+    def set_category(self, category:int):
+        self.category = category
+
+    def set_length(self, length:int):
+        self.length = length
+
+    def set_state(self, state:int):
+        self.state = state
+
+    def set_regulation(self, regulation):
+        self.regulation = regulation
+
+    def add_tags(self, tag:str):
+        self.tags.append(tag)
+
+    def publish(self):
+        self.isPublished = True
+
+
+
 class Novel():
-    class BasicInfo():
-        def __init__(self):
-            self.title:str = ''
-            self.description:str = ''
-
-            # 0: no selection  # 1: for men  # 2: for wemen
-            self.genre:int = 1
-            
-            # 110100: mistery   # 110200: horror     # 110300: SF       # 110400: fantasy
-            # 110500: love      # 111400: character  # 111500: light    # 110600: blue-spring
-            # 110700: current   # 110800: masses     # 111000: economy  # 111100: history
-            # 111200: children  # 111300: picture    # 119000: BL       # 111600: essay
-            self.category:int = 110400
-
-            # 1: short-short  # 2: short  # 3: long
-            self.length = 3
-
-            # 1: completed  # 2: writing now
-            self.state = 2
-
-            # 0: no regulation  # 1: R-15  # 2: R-18
-            self.regulation = 0
-
-            self.tags:list[str] = []
-
-            self.isPublished:bool = False
+    basicInfo:BasicInfo
+    stories:list[Story]
 
 
-    class StoryInfo():
-        def __init__(self):
-            pass
